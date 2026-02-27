37a067215c622bbbd9764d66f430875a17e2ffc0
Oleg Koval <oleg@bizcuit.nl>
2026-02-27 13:53:21 +0100

    Release v1.0.0

--- cmd/root.go
+++ cmd/root.go
@@ -28,7 +28,7 @@ import (
 	"github.com/oleg-koval/promptctl/prompt"
 )
 
-const version = "0.9.5"
+const version = "1.0.0"
 
 const githubReleasesLatest = "https://api.github.com/repos/oleg-koval/promptctl/releases/latest"
 const versionCheckInterval = 24 * time.Hour
